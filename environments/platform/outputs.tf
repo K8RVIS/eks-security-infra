ab597d9d8661acca73a6da3a2baa09c5cca5a1ef
esc <eunso02@sookmyung.ac.kr>
2026-04-03 17:59:27 +0900

    feat: 애드온과 플랫폼 연결

--- environments/platform/outputs.tf
+++ environments/platform/outputs.tf
@@ -12,3 +12,28 @@ output "platform_state_ready" {
   description = "Whether the platform root is configured to consume the infra state."
   value       = true
 }
+
+output "metrics_server_release_name" {
+  description = "Helm release name for Metrics Server."
+  value       = module.k8s_base.metrics_server_release_name
+}
+
+output "aws_node_termination_handler_release_name" {
+  description = "Helm release name for AWS Node Termination Handler."
+  value       = module.k8s_base.aws_node_termination_handler_release_name
+}
+
+output "ingress_release_name" {
+  description = "Helm release name for ingress-nginx."
+  value       = module.k8s_base.ingress_release_name
+}
+
+output "ingress_namespace" {
+  description = "Namespace where ingress-nginx is deployed."
+  value       = module.k8s_base.ingress_namespace
+}
+
+output "ingress_service_name" {
+  description = "Service name exposed by ingress-nginx."
+  value       = module.k8s_base.ingress_service_name
+}
--- environments/platform/variables.tf
+++ environments/platform/variables.tf
@@ -28,3 +28,51 @@ variable "infra_state_region" {
   description = "AWS region of the S3 bucket that stores the infra Terraform state."
   type        = string
 }
+
+variable "helm_release_timeout_seconds" {
+  description = "Timeout in seconds applied to each addon helm release."
+  type        = number
+  default     = 600
+}
+
+variable "metrics_server_namespace" {
+  description = "Namespace used for the Metrics Server release."
+  type        = string
+  default     = "kube-system"
+}
+
+variable "metrics_server_chart_version" {
+  description = "Pinned chart version for Metrics Server."
+  type        = string
+  default     = "3.13.0"
+}
+
+variable "aws_node_termination_handler_namespace" {
+  description = "Namespace used for the AWS Node Termination Handler release."
+  type        = string
+  default     = "kube-system"
+}
+
+variable "aws_node_termination_handler_chart" {
+  description = "Chart name for the AWS Node Termination Handler OCI repository."
+  type        = string
+  default     = "aws-node-termination-handler"
+}
+
+variable "aws_node_termination_handler_chart_version" {
+  description = "Pinned chart version for AWS Node Termination Handler. Set to null to use the repository default."
+  type        = string
+  default     = null
+}
+
+variable "ingress_nginx_namespace" {
+  description = "Namespace used for the ingress-nginx release."
+  type        = string
+  default     = "ingress-nginx"
+}
+
+variable "ingress_nginx_chart_version" {
+  description = "Pinned chart version for ingress-nginx."
+  type        = string
+  default     = "4.14.1"
+}
