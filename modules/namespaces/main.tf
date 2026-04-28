f215dabcb1fece84035390e510070da89f3cc579
kyum0731 <yunkyumnam@gmail.com>
2026-04-28 15:57:11 +0900

    feat: cluster resource 사용제한

--- modules/namespaces/main.tf
+++ modules/namespaces/main.tf
@@ -9,3 +9,36 @@ resource "kubernetes_namespace_v1" "teams" {
     }
   }
 }
+
+variable "default_namespace_resource_quota_hard" {
+  description = "Default hard resource quota applied to the limited namespace."
+  type        = map(string)
+
+  default = {
+    pods              = "20"
+    "requests.cpu"    = "2"
+    "requests.memory" = "4Gi"
+    "limits.cpu"      = "4"
+    "limits.memory"   = "8Gi"
+  }
+}
+
+variable "namespace_container_default_requests" {
+  description = "Default container resource requests applied by LimitRange."
+  type        = map(string)
+
+  default = {
+    cpu    = "100m"
+    memory = "128Mi"
+  }
+}
+
+variable "namespace_container_default_limits" {
+  description = "Default container resource limits applied by LimitRange."
+  type        = map(string)
+
+  default = {
+    cpu    = "500m"
+    memory = "512Mi"
+  }
+}
--- modules/namespaces/variables.tf
+++ modules/namespaces/variables.tf
@@ -8,3 +8,36 @@ variable "team_names" {
   type        = list(string)
   default     = ["team-a", "team-b", "team-c", "team-d"]
 }
+
+variable "default_namespace_resource_quota_hard" {
+  description = "Default hard resource quota applied to each team namespace."
+  type        = map(string)
+
+  default = {
+    pods              = "20"
+    "requests.cpu"    = "2"
+    "requests.memory" = "4Gi"
+    "limits.cpu"      = "4"
+    "limits.memory"   = "8Gi"
+  }
+}
+
+variable "namespace_container_default_requests" {
+  description = "Default container resource requests applied by LimitRange."
+  type        = map(string)
+
+  default = {
+    cpu    = "100m"
+    memory = "128Mi"
+  }
+}
+
+variable "namespace_container_default_limits" {
+  description = "Default container resource limits applied by LimitRange."
+  type        = map(string)
+
+  default = {
+    cpu    = "500m"
+    memory = "512Mi"
+  }
+}
