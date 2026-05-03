d4fdb66b96d3acb8f63c9f6b4aa831a191f4864c
jhy <152116990+kiiiiy@users.noreply.github.com>
2026-05-03 19:20:49 +0900

    누락 부분 추가

--- modules/namespaces/main.tf
+++ modules/namespaces/main.tf
@@ -3,9 +3,15 @@ resource "kubernetes_namespace_v1" "teams" {
 
   metadata {
     name = each.value
-    labels = {
-      "app.kubernetes.io/part-of" = var.project_name
-      "training.k8rvis.io/team"   = each.value
-    }
+    labels = merge(
+      {
+        "app.kubernetes.io/part-of" = var.project_name
+        "training.k8rvis.io/team"   = each.value
+      },
+      each.value == "team-b" ? {
+        "pod-security.kubernetes.io/enforce"         = "restricted"
+        "pod-security.kubernetes.io/enforce-version" = "latest"
+      } : {}
+    )
   }
 }
