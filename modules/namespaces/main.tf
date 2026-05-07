5414c380e230a8cd232055770d3eef433995a72d
kiiiiy <mbusiness0917@gmail.com>
2026-05-07 14:51:18 +0900

    pss baseline

--- modules/namespaces/main.tf
+++ modules/namespaces/main.tf
@@ -3,16 +3,16 @@ resource "kubernetes_namespace_v1" "teams" {
 
   metadata {
     name = each.value
-    labels = merge(
-      {
-        "app.kubernetes.io/part-of" = var.project_name
-        "training.k8rvis.io/team"   = each.value
-      },
-      each.value == "team-b" ? {
-        "pod-security.kubernetes.io/enforce"         = "restricted"
-        "pod-security.kubernetes.io/enforce-version" = "latest"
-      } : {}
-    )
+    labels = {
+      "app.kubernetes.io/part-of"                  = var.project_name
+      "training.k8rvis.io/team"                    = each.value
+      "pod-security.kubernetes.io/enforce"         = "baseline"
+      "pod-security.kubernetes.io/enforce-version" = "latest"
+      "pod-security.kubernetes.io/audit"           = "baseline"
+      "pod-security.kubernetes.io/audit-version"   = "latest"
+      "pod-security.kubernetes.io/warn"            = "baseline"
+      "pod-security.kubernetes.io/warn-version"    = "latest"
+    }
   }
 }
 
